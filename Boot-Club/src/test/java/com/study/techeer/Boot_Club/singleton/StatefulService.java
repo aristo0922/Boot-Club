5f0a2e89caeabe5e9280a23a75e71361362addf1
aristo <dkfud2121@naver.com>
2024-10-22 20:36:14 +0900

    section.6 chapter.37 싱글톤 방식의 주의점

--- Boot-Club/src/test/java/com/study/techeer/Boot_Club/singleton/StatefulService.java
+++ Boot-Club/src/test/java/com/study/techeer/Boot_Club/singleton/StatefulService.java
@@ -4,13 +4,14 @@ public class StatefulService {
 
   private int price; // 상태를 유지하는 필드
 
-  public void order(String name, int price){
+  public int order(String name, int price){
     System.out.println("name = " + name+ " price = " + price);
-    this.price =price; // 문제되는 부분
+    return price;
+//    this.price =price; // 문제되는 부분
   }
 
-  public int getPrice(){
-    return price;
-  }
+//  public int getPrice(){
+//    return price;
+//  }
 
 }
