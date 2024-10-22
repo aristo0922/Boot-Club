2a863705fdfec5746af234381f955830e342fdd3
aristo <dkfud2121@naver.com>
2024-10-22 22:47:30 +0900

    fix conflict

--- Boot-Club/src/test/java/com/study/techeer/Boot_Club/singleton/ConfigurationSingletonTest.java
+++ Boot-Club/src/test/java/com/study/techeer/Boot_Club/singleton/ConfigurationSingletonTest.java
@@ -17,12 +17,14 @@ public class ConfigurationSingletonTest {
 
     MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
     OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+    MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
     MemberRepository memberRepository1 = memberService.getMemberRepository();
     MemberRepository memberRepository2 = orderService.getMemberRepository();
 
     System.out.println("memberSerivce -> memberRepository1 = " + memberRepository1);
     System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+    System.out.println("memberRepository = " + memberRepository);
 
     Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
   }
