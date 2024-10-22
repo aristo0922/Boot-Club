8907a652d01150d6c357bc8a523570535ea53b20
aristo <dkfud2121@naver.com>
2024-10-22 20:31:15 +0900

    공유자원 문제 발생

---
+++ Boot-Club/src/test/java/com/study/techeer/Boot_Club/singleton/StatefulService.java
@@ -0,0 +1,16 @@
+package com.study.techeer.Boot_Club.singleton;
+
+public class StatefulService {
+
+  private int price; // 상태를 유지하는 필드
+
+  public void order(String name, int price){
+    System.out.println("name = " + name+ " price = " + price);
+    this.price =price; // 문제되는 부분
+  }
+
+  public int getPrice(){
+    return price;
+  }
+
+}
