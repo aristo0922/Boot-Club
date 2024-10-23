662a5a2d058b6b177bbcd313caa8b7c7e8ceb45c
aristo <dkfud2121@naver.com>
2024-10-23 17:35:38 +0900

    section.7 chapter.43 중복 등록과 충돌

--- Boot-Club/src/main/java/com/study/techeer/Boot_Club/AutoAppConfig.java
+++ Boot-Club/src/main/java/com/study/techeer/Boot_Club/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+    basePackages = "com.study.techeer.Boot_Club.member",
+    basePackageClasses = AutoAppConfig.class,
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
---
+++ Boot-Club/src/test/java/com/study/techeer/Boot_Club/scan/filter/BeanA.java
@@ -0,0 +1,6 @@
+package com.study.techeer.Boot_Club.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+
+}
