bbc06ad4f8b61876f7c6fc20194df02387eb487d
aristo <dkfud2121@naver.com>
2024-10-30 15:26:46 +0900

    chapter 56 annotation

--- Boot-Club/src/test/java/com/study/techeer/Boot_Club/lifecycle/NetworkClient.java
+++ Boot-Club/src/test/java/com/study/techeer/Boot_Club/lifecycle/NetworkClient.java
@@ -1,5 +1,7 @@
 package com.study.techeer.Boot_Club.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
@@ -33,12 +35,14 @@ public class NetworkClient {
   }
 
   // 의존 관계 연결을 마치면
+  @PostConstruct
   public void init(){
     System.out.println("NetworkClient.init");
     connect();
     call("초기화 연결 메시지");
   }
 
+  @PreDestroy
   public void close(){
     System.out.println("networkclient.close");
     disconnect();
